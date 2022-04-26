56bd5dcbf03950a89c8ae4ba198b5064dc2fdabd
heyeman <abdisaheyeman083@gmail.com>
2022-04-26 22:49:37 +0300

    improved

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     def list_all(mongo_collection):
         """list all documents in a collection"""
         documents = []
-        res = mongo_collection.find({})
+        res = mongo_collection.find()
         for i in res:
             documents.append(i)
         return documents
