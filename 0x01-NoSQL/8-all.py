185b649d13dd96464cb800d181c160a15982059c
heyeman <abdisaheyeman083@gmail.com>
2022-04-26 22:51:59 +0300

    improved2

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""my first NoSQL database"""
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """list all documents in a collection"""
-        return [i for i in mongo_collection.find()]
+""" Lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """ Return a list of all documents in a collection or an empty list
+    """
+    return [each for each in mongo_collection.find()]
